d6b95e47305dbafbeef9ef2a1a850f498aa75217
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-02-27 10:20:14 -0300

    feat: configure project

---
+++ app/components/nav/nav.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function Nav() {
+    return ( 
+        <div>
+            <Button>Sign In</Button>
+        </div>
+     );
+}
+
+export default Nav;
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Nav from "./components/nav/nav";
+
+export default function Home() {
+  return (
+    <main className="max-w-[1280px] mx-auto">
+      <Nav />
+  </main>
+  );
+}
