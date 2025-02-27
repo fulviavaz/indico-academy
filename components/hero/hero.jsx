5dd0fde0b4a6dc5948212b8304eb64b82ab0947f
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-02-27 11:46:39 -0300

    fix: line height

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return ( 
         <div className="h-[612px] bg-[url('/images/hero-academy.png')] bg-cover bg-center no-repeat">
             <div className="flex flex-col items-start justify-center pl-[60px] py-24 w-[985px]">
-                <h1 className="text-[64px] font-bold text-white">Transforme o relacionamento com os clientes criando uma
+                <h1 className="text-[64px] font-bold text-white leading-tight">Transforme o relacionamento com os clientes criando uma
                     conexão legítima</h1>
                 <p className="mt-4 text-[20px] text-white w-[891px]">Com palestras e workshops práticos, a Indico Academy prepara sua empresa para
                     construir um relacionamento mais profundo e duradouro com seus consumidores.
