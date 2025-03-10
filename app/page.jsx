2e33501bfbc6a79909d445ba7395b80f671ac4b9
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-03-10 18:41:38 -0300

    fix: responsivenes

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import Subjects from "@/components/subjects/subjects";
 
 export default function Home() {
   return (
-    <main className="max-w-[1280px] mx-auto">
+    <main className="max-w-[1280px] mx-auto items-center justify-center">
       <Nav />
       <Hero />
       <Speakers />
--- components/speakers/speakers.jsx
+++ components/speakers/speakers.jsx
@@ -3,16 +3,17 @@ import SpeakerCard from "../speaker-card/speaker-card";
 function Speakers() {
     return ( 
         <div className="py-20">
-            <div className="flex flex-col justify-start pl-[60px]">
-                <h2 className="text-[64px] font-normal">Palestrantes</h2>
-                <p className="text-[15px]">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex. vitae Ut Vestibulum</p>
-            </div>
-            <div className="flex justify-between mt-10">
+                    <div className="flex flex-col justify-start pl-[60px]">
+                        <h2 className="text-[64px] font-normal">Palestrantes</h2>
+                        <p className="text-[15px]">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex. vitae Ut Vestibulum</p>
+                    </div>
+                    <div className="mt-10 flex gap-8 justify-center">
+                        <SpeakerCard image="/images/ranoya.png" name="Christiano Ranoya" />
                 <SpeakerCard image="/images/ranoya.png" name="Christiano Ranoya" />
                 <SpeakerCard image="/images/ranoya.png" name="Christiano Ranoya" />
-                <SpeakerCard image="/images/ranoya.png" name="Christiano Ranoya" />
-            </div>
-        </div>
+                    </div>
+                </div>
+        
      );
 }
 
