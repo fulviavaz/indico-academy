88ed3aa0b565dc37df49de32dbc6c8184db3f1e9
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-03-17 16:11:35 -0300

    feat: fix gaps

--- components/modalities/modalities.jsx
+++ components/modalities/modalities.jsx
@@ -2,8 +2,10 @@ function Modalities() {
     return ( 
         <div className="pb-10">
                     <div className="flex flex-col justify-start  2xl:pl-[60px] lg:pl-[60px] md:pl-[30px] pl-[30px]">
-                        <h2 className="2xl:text-[64px] lg:text-[64px] md:text-[45px] text-[35px]  font-normal text-white">Modalidades</h2>
-                        <p className="text-[15px] text-white">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex. vitae Ut Vestibulum</p>
+                        <h2 className="2xl:text-[64px] lg:text-[64px] md:text-[45px] text-[35px] font-normal text-white font-display">COMO FUNCIONA</h2>
+                <div className="w-full h-[1px] bg-[#FF1234]"></div>
+                <p className="text-[24px] text-white font-body mt-6">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex.
+                    vitae Ut Vestibulum</p>
                     </div>
                     <div className="mt-10 flex flex-col items-center gap-6">
                 <div className="h-[339px] 2xl:w-[1155px] lg:w-[900px] md:w-[700px] w-[300px] bg-[url('/images/palestra1.png')] bg-cover bg-center no-repeat transition-transform duration-300 hover:scale-105 rounded-lg"></div>
--- components/speakers/speakers.jsx
+++ components/speakers/speakers.jsx
@@ -6,13 +6,10 @@ function Speakers() {
                     <div className="flex flex-col justify-start 2xl:pl-[60px] lg:pl-[60px] md:pl-[30px] pl-[30px]">
                         <h2 className="2xl:text-[64px] lg:text-[64px] md:text-[45px] text-[35px] font-normal text-white font-display">PALESTRANTES</h2>
                 <div className="w-full h-[1px] bg-[#FF1234]"></div>
-                <p className="text-[24px] text-white font-body mt-6">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex. vitae Ut Vestibulum 
-
-
-
-</p>
+                <p className="text-[24px] text-white font-body mt-6">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex.
+                    vitae Ut Vestibulum</p>
                     </div>
-                    <div className="mt-10 flex gap-8 justify-center flex-wrap">
+                    <div className="mt-10 flex gap-6 justify-center flex-wrap">
                         <SpeakerCard image="/images/ranoya.png" name="Christiano Ranoya" />
                 <SpeakerCard image="/images/ranoya.png" name="Christiano Ranoya" />
                 <SpeakerCard image="/images/ranoya.png" name="Christiano Ranoya" />
