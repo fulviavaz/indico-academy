d709309aa6bebfbfd3b71de4f0ca9b312c03a073
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-03-19 15:50:00 -0300

    feat: award resposiveness

--- components/award/award.jsx
+++ components/award/award.jsx
@@ -1,17 +1,17 @@
 function Award() {
     return ( 
-        <div className="pb-10">
-                    <div className="flex flex-col justify-start 2xl:pl-[60px] lg:pl-[60px] md:pl-[30px] pl-[30px]">
+        <div className="pb-20">
+                    <div className="flex flex-col justify-start 2xl:px-[60px] xl:px-[60px] lg:px-[60px] md:px-[30px] px-[30px]">
                         <h2 className="2xl:text-[64px] lg:text-[64px] md:text-[45px] text-[35px] font-normal text-white font-display">O QUE SUA EMPRESA GANHA</h2>
                         <div className="w-full h-[1px] bg-[#FF1234]"></div>
-                        <p className="text-[24px] text-white font-body mt-6">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex.
+                        <p className="w-ful 2xl:text-[24px] xl:text-[24px] lg:text-[24px] md:text-[24px] text-[18px] text-white font-body mt-6">Sed diam consectetur non non Morbi massa Praesent ex Ut nec ex.
                             vitae Ut Vestibulum</p>
             </div>
-            <div className="mt-10 flex items-center justify-center gap-6">
+            <div className="mt-10 flex items-center justify-center gap-6 flex-wrap">
 
                 <div className="relative w-[367px] h-[412px] rounded-lg overflow-hidden  transition-transform duration-300 hover:scale-105">
   <div className="absolute inset-0 bg-black opacity-50"></div>
-                    <div className="bg-[url('/images/ranoya.png')] bg-cover bg-center no-repeat w-full h-full p-10">
+                    <div className="bg-[url('/images/aw1.jpg')] bg-cover bg-center no-repeat w-full h-full p-10">
                         <p className="font-body text-white text-[20px] font-bold absolute">Maior Engajamento<br /> e Retenção de Clientes</p>
                         <p className="font-body text-white text-[18px] font-normal absolute mt-28 w-[274px]">Ajudamos sua equipe a entender o que realmente importa para os seus clientes,
                             proporcionando soluções práticas e estratégias que irão aumentar a fidelização e o valor dos seus clientes ao longo do tempo.</p>
@@ -20,7 +20,7 @@ function Award() {
                 
                     <div className="relative w-[367px] h-[412px] rounded-lg overflow-hidden  transition-transform duration-300 hover:scale-105">
   <div className="absolute inset-0 bg-black opacity-50"></div>
-                    <div className="bg-[url('/images/ranoya.png')] bg-cover bg-center no-repeat w-full h-full p-10">
+                    <div className="bg-[url('/images/aw2.jpg')] bg-cover bg-center no-repeat w-full h-full p-10">
                         <p className="font-body text-white text-[20px] font-bold absolute">Capacitação Profunda</p>
                         <p className="font-body text-white text-[18px] font-normal absolute mt-28 w-[274px]">‍‍Você não só vai apresentar conceitos para sua equipe, mas também estimulará a ação,
                             incentivando a implementação imediata de soluções no contexto específico de sua empresa.</p>
@@ -29,7 +29,7 @@ function Award() {
                 
                     <div className="relative w-[367px] h-[412px] rounded-lg overflow-hidden  transition-transform duration-300 hover:scale-105">
   <div className="absolute inset-0 bg-black opacity-50"></div>
-                    <div className="bg-[url('/images/ranoya.png')] bg-cover bg-center no-repeat w-full h-full p-10">
+                    <div className="bg-[url('/images/aw3.jpg')] bg-cover bg-center no-repeat w-full h-full p-10">
                         <p className="font-body text-white text-[20px] font-bold absolute">Resultados Tangíveis</p>
                         <p className="font-body text-white text-[18px] font-normal absolute mt-28 w-[274px]">Com nossa metodologia prática, as palestras e workshops não são
                             apenas momentos de aprendizado, mas de transformação. Seus projetos vão avançar, e os resultados vão aparecer.</p>
