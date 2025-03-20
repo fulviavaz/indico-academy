2744c20797ba49d3e518753b25df89ef7e357b07
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-03-20 16:05:46 -0300

    feat: udape modalities

--- components/modalities/modalities.jsx
+++ components/modalities/modalities.jsx
@@ -6,9 +6,31 @@ function Modalities() {
                 <div className="w-full h-[1px] bg-[#FF1234]"></div>
                 <p className="w-ful 2xl:text-[24px] xl:text-[24px] lg:text-[24px] md:text-[24px] text-[18px] text-white font-body mt-6">Palestras e workshops para capacitar sua equipe e aplicar estratégias de Loyalty</p>
                     </div>
-                    <div className="mt-10 flex flex-col items-center gap-6">
-                <div className="h-[339px] 2xl:w-[1155px] xl:w-[1155px] lg:w-[900px] md:w-[700px] w-[300px] bg-[url('/images/palestra1.png')] bg-cover bg-center no-repeat transition-transform duration-300 hover:scale-105 rounded-lg"></div>
-                <div className="h-[339px] 2xl:w-[1155px] xl:w-[1155px] lg:w-[900px] md:w-[700px] w-[300px] bg-[url('/images/palestra2.png')] bg-cover bg-center no-repeat transition-transform duration-300 hover:scale-105 rounded-lg"></div>
+            <div className="mt-10 flex items-center justify-center flex-wrap gap-6">
+                
+                <div className="relative 2xl:w-[557px] xl:w-[557px] lg:w-[557px] md:w-[350px] w-[300px] h-[339px] rounded-lg overflow-hidden  transition-transform duration-300 hover:scale-105">
+  <div className="absolute inset-0 bg-black opacity-50"></div>
+                    <div className="bg-[url('/images/palestra1.png')] bg-cover bg-center no-repeat w-full h-full 2xl:p-10 xl:p-10 lg:p-10 md:p-10 p-5">
+                        <p className="font-body text-white text-[20px] font-bold absolute">Palestras</p>
+                        <p className="font-body text-white text-[18px] font-normal absolute mt-14 2xl:w-[350px] xl:w-[350px] lg:w-[350px] md:w-[350px] w-[90%]">
+                            O foco da palestra é oferecer um conteúdo impactante e inspirador, transmitindo conhecimento de forma clara e objetiva, 
+                            para provocar reflexões 
+                            e despertar o interesse por novas abordagens no relacionamento com clientes.
+                            </p>
+                    </div>                    
+                </div>
+
+                <div className="relative 2xl:w-[557px] xl:w-[557px] lg:w-[557px] md:w-[350px] w-[300px] h-[339px] rounded-lg overflow-hidden  transition-transform duration-300 hover:scale-105">
+  <div className="absolute inset-0 bg-black opacity-50"></div>
+                    <div className="bg-[url('/images/palestra2.jpg')] bg-cover bg-center no-repeat w-full h-full 2xl:p-10 xl:p-10 lg:p-10 md:p-10 p-5">
+                        <p className="font-body text-white text-[20px] font-bold absolute">Workshops</p>
+                        <p className="font-body text-white text-[18px] font-normal absolute mt-14 2xl:w-[350px] xl:w-[350px] lg:w-[350px] md:w-[350px] w-[90%]">
+                            O workshop é desenhado para proporcionar uma experiência prática e colaborativa, permitindo que os participantes apliquem conceitos e trabalhem em soluções reais para seus desafios de relacionamento com clientes.
+
+                            </p>
+                    </div>                    
+                </div>
+                
                     </div>
                 </div>
     );
